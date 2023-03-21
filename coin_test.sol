9339abd6741225265206b9698ca91a84c30ae05a
Vika6666 <128487692+Vika6666@users.noreply.github.com>
2023-03-21 17:27:18 +0300

    Create coin_test.sol

---
+++ coin_test.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public constant name = "MyToken";
+    string public constant symbol = "MTK";
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply;
+        balances[msg.sender] = initialSupply;
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        return balances[owner];
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+    
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+
+
+        balances[from] -= value;
+        balances[to] += value;
+        allowances[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return allowances[owner][spender];
+    }
+}
